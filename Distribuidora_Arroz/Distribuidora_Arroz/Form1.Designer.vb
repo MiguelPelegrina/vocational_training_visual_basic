f616025a1732fe76ab91d85657afa702a58a35d8
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2023-01-15 16:04:43 +0100

    Add files via upload

---
+++ Distribuidora_Arroz/Distribuidora_Arroz/Form1.Designer.vb
@@ -0,0 +1,331 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TimerDiaHora = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTiempo = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.lblPesoSacas = New System.Windows.Forms.Label()
+        Me.tbPesoSacas = New System.Windows.Forms.TrackBar()
+        Me.lbCamiones = New System.Windows.Forms.ListBox()
+        Me.ilCamiones = New System.Windows.Forms.ImageList(Me.components)
+        Me.pbImagenesCamiones = New System.Windows.Forms.PictureBox()
+        Me.lblPesoCamion = New System.Windows.Forms.Label()
+        Me.btnCargarSaca = New System.Windows.Forms.Button()
+        Me.pbSacas = New System.Windows.Forms.PictureBox()
+        Me.ilSacas = New System.Windows.Forms.ImageList(Me.components)
+        Me.pbSaca = New System.Windows.Forms.ProgressBar()
+        Me.TimerSaca = New System.Windows.Forms.Timer(Me.components)
+        Me.lblAtencion = New System.Windows.Forms.Label()
+        Me.lblNumeroCamion = New System.Windows.Forms.Label()
+        Me.lblNumeroSaca = New System.Windows.Forms.Label()
+        Me.btnCargarCamion = New System.Windows.Forms.Button()
+        Me.lblPesoCamionActual = New System.Windows.Forms.Label()
+        Me.gbInformacionCamion = New System.Windows.Forms.GroupBox()
+        Me.gbInformacionSaca = New System.Windows.Forms.GroupBox()
+        Me.gbInformacion = New System.Windows.Forms.GroupBox()
+        CType(Me.tbPesoSacas, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbImagenesCamiones, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbSacas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbInformacionCamion.SuspendLayout()
+        Me.gbInformacionSaca.SuspendLayout()
+        Me.gbInformacion.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TimerDiaHora
+        '
+        Me.TimerDiaHora.Enabled = True
+        Me.TimerDiaHora.Interval = 1000
+        '
+        'lblTiempo
+        '
+        Me.lblTiempo.AutoSize = True
+        Me.lblTiempo.Location = New System.Drawing.Point(4, 16)
+        Me.lblTiempo.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblTiempo.Name = "lblTiempo"
+        Me.lblTiempo.Size = New System.Drawing.Size(76, 15)
+        Me.lblTiempo.TabIndex = 0
+        Me.lblTiempo.Text = "                       "
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(4, 31)
+        Me.LinkLabel1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(132, 15)
+        Me.LinkLabel1.TabIndex = 1
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Página web empresarial"
+        '
+        'lblPesoSacas
+        '
+        Me.lblPesoSacas.AutoSize = True
+        Me.lblPesoSacas.Location = New System.Drawing.Point(258, 16)
+        Me.lblPesoSacas.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblPesoSacas.Name = "lblPesoSacas"
+        Me.lblPesoSacas.Size = New System.Drawing.Size(109, 15)
+        Me.lblPesoSacas.TabIndex = 3
+        Me.lblPesoSacas.Text = "Peso de sacas: 6000"
+        '
+        'tbPesoSacas
+        '
+        Me.tbPesoSacas.Location = New System.Drawing.Point(258, 33)
+        Me.tbPesoSacas.Margin = New System.Windows.Forms.Padding(2)
+        Me.tbPesoSacas.Maximum = 9000
+        Me.tbPesoSacas.Minimum = 3000
+        Me.tbPesoSacas.Name = "tbPesoSacas"
+        Me.tbPesoSacas.Size = New System.Drawing.Size(109, 45)
+        Me.tbPesoSacas.TabIndex = 4
+        Me.tbPesoSacas.Value = 6000
+        '
+        'lbCamiones
+        '
+        Me.lbCamiones.FormattingEnabled = True
+        Me.lbCamiones.ItemHeight = 15
+        Me.lbCamiones.Items.AddRange(New Object() {"Camión pequeño", "Camión mediano", "Camión grande"})
+        Me.lbCamiones.Location = New System.Drawing.Point(4, 18)
+        Me.lbCamiones.Margin = New System.Windows.Forms.Padding(2)
+        Me.lbCamiones.Name = "lbCamiones"
+        Me.lbCamiones.Size = New System.Drawing.Size(204, 49)
+        Me.lbCamiones.TabIndex = 5
+        '
+        'ilCamiones
+        '
+        Me.ilCamiones.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ilCamiones.ImageStream = CType(resources.GetObject("ilCamiones.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ilCamiones.TransparentColor = System.Drawing.Color.Transparent
+        Me.ilCamiones.Images.SetKeyName(0, "camionpequeno.jpg")
+        Me.ilCamiones.Images.SetKeyName(1, "camionmediano.jpg")
+        Me.ilCamiones.Images.SetKeyName(2, "camiongrande.jpg")
+        '
+        'pbImagenesCamiones
+        '
+        Me.pbImagenesCamiones.Location = New System.Drawing.Point(372, 357)
+        Me.pbImagenesCamiones.Margin = New System.Windows.Forms.Padding(2)
+        Me.pbImagenesCamiones.Name = "pbImagenesCamiones"
+        Me.pbImagenesCamiones.Size = New System.Drawing.Size(250, 250)
+        Me.pbImagenesCamiones.TabIndex = 6
+        Me.pbImagenesCamiones.TabStop = False
+        '
+        'lblPesoCamion
+        '
+        Me.lblPesoCamion.AutoSize = True
+        Me.lblPesoCamion.Location = New System.Drawing.Point(4, 67)
+        Me.lblPesoCamion.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblPesoCamion.Name = "lblPesoCamion"
+        Me.lblPesoCamion.Size = New System.Drawing.Size(174, 15)
+        Me.lblPesoCamion.TabIndex = 7
+        Me.lblPesoCamion.Text = "Peso máximo del camión: 0       "
+        Me.lblPesoCamion.Visible = False
+        '
+        'btnCargarSaca
+        '
+        Me.btnCargarSaca.Enabled = False
+        Me.btnCargarSaca.Location = New System.Drawing.Point(4, 16)
+        Me.btnCargarSaca.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnCargarSaca.Name = "btnCargarSaca"
+        Me.btnCargarSaca.Size = New System.Drawing.Size(85, 30)
+        Me.btnCargarSaca.TabIndex = 8
+        Me.btnCargarSaca.Text = "Cargar saca"
+        Me.btnCargarSaca.UseVisualStyleBackColor = True
+        '
+        'pbSacas
+        '
+        Me.pbSacas.Location = New System.Drawing.Point(4, 50)
+        Me.pbSacas.Margin = New System.Windows.Forms.Padding(2)
+        Me.pbSacas.Name = "pbSacas"
+        Me.pbSacas.Size = New System.Drawing.Size(250, 250)
+        Me.pbSacas.TabIndex = 9
+        Me.pbSacas.TabStop = False
+        '
+        'ilSacas
+        '
+        Me.ilSacas.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ilSacas.ImageStream = CType(resources.GetObject("ilSacas.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ilSacas.TransparentColor = System.Drawing.Color.Transparent
+        Me.ilSacas.Images.SetKeyName(0, "sacaLlena.jpg")
+        Me.ilSacas.Images.SetKeyName(1, "pala.jpg")
+        '
+        'pbSaca
+        '
+        Me.pbSaca.Location = New System.Drawing.Point(4, 319)
+        Me.pbSaca.Margin = New System.Windows.Forms.Padding(2)
+        Me.pbSaca.Name = "pbSaca"
+        Me.pbSaca.Size = New System.Drawing.Size(105, 20)
+        Me.pbSaca.TabIndex = 10
+        Me.pbSaca.Visible = False
+        '
+        'TimerSaca
+        '
+        '
+        'lblAtencion
+        '
+        Me.lblAtencion.AutoSize = True
+        Me.lblAtencion.Location = New System.Drawing.Point(4, 302)
+        Me.lblAtencion.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblAtencion.Name = "lblAtencion"
+        Me.lblAtencion.Size = New System.Drawing.Size(120, 15)
+        Me.lblAtencion.TabIndex = 11
+        Me.lblAtencion.Text = "Atención: cargando..."
+        Me.lblAtencion.Visible = False
+        '
+        'lblNumeroCamion
+        '
+        Me.lblNumeroCamion.AutoSize = True
+        Me.lblNumeroCamion.Location = New System.Drawing.Point(4, 50)
+        Me.lblNumeroCamion.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblNumeroCamion.Name = "lblNumeroCamion"
+        Me.lblNumeroCamion.Size = New System.Drawing.Size(76, 15)
+        Me.lblNumeroCamion.TabIndex = 12
+        Me.lblNumeroCamion.Text = "Camion nº 0 "
+        '
+        'lblNumeroSaca
+        '
+        Me.lblNumeroSaca.AutoSize = True
+        Me.lblNumeroSaca.Location = New System.Drawing.Point(4, 67)
+        Me.lblNumeroSaca.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblNumeroSaca.Name = "lblNumeroSaca"
+        Me.lblNumeroSaca.Size = New System.Drawing.Size(55, 15)
+        Me.lblNumeroSaca.TabIndex = 13
+        Me.lblNumeroSaca.Text = "Saca nº 0"
+        '
+        'btnCargarCamion
+        '
+        Me.btnCargarCamion.Enabled = False
+        Me.btnCargarCamion.Location = New System.Drawing.Point(4, 84)
+        Me.btnCargarCamion.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnCargarCamion.Name = "btnCargarCamion"
+        Me.btnCargarCamion.Size = New System.Drawing.Size(96, 26)
+        Me.btnCargarCamion.TabIndex = 14
+        Me.btnCargarCamion.Text = "Cargar camion"
+        Me.btnCargarCamion.UseVisualStyleBackColor = True
+        '
+        'lblPesoCamionActual
+        '
+        Me.lblPesoCamionActual.AutoSize = True
+        Me.lblPesoCamionActual.Location = New System.Drawing.Point(4, 112)
+        Me.lblPesoCamionActual.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblPesoCamionActual.Name = "lblPesoCamionActual"
+        Me.lblPesoCamionActual.Size = New System.Drawing.Size(141, 15)
+        Me.lblPesoCamionActual.TabIndex = 15
+        Me.lblPesoCamionActual.Text = "Peso actual del camion: 0"
+        '
+        'gbInformacionCamion
+        '
+        Me.gbInformacionCamion.Controls.Add(Me.lbCamiones)
+        Me.gbInformacionCamion.Controls.Add(Me.lblPesoCamionActual)
+        Me.gbInformacionCamion.Controls.Add(Me.lblPesoCamion)
+        Me.gbInformacionCamion.Controls.Add(Me.btnCargarCamion)
+        Me.gbInformacionCamion.Location = New System.Drawing.Point(151, 7)
+        Me.gbInformacionCamion.Margin = New System.Windows.Forms.Padding(2)
+        Me.gbInformacionCamion.Name = "gbInformacionCamion"
+        Me.gbInformacionCamion.Padding = New System.Windows.Forms.Padding(2)
+        Me.gbInformacionCamion.Size = New System.Drawing.Size(216, 145)
+        Me.gbInformacionCamion.TabIndex = 16
+        Me.gbInformacionCamion.TabStop = False
+        Me.gbInformacionCamion.Text = "Información del camión"
+        '
+        'gbInformacionSaca
+        '
+        Me.gbInformacionSaca.Controls.Add(Me.lblPesoSacas)
+        Me.gbInformacionSaca.Controls.Add(Me.btnCargarSaca)
+        Me.gbInformacionSaca.Controls.Add(Me.pbSacas)
+        Me.gbInformacionSaca.Controls.Add(Me.tbPesoSacas)
+        Me.gbInformacionSaca.Controls.Add(Me.pbSaca)
+        Me.gbInformacionSaca.Controls.Add(Me.lblAtencion)
+        Me.gbInformacionSaca.Location = New System.Drawing.Point(372, 7)
+        Me.gbInformacionSaca.Margin = New System.Windows.Forms.Padding(2)
+        Me.gbInformacionSaca.Name = "gbInformacionSaca"
+        Me.gbInformacionSaca.Padding = New System.Windows.Forms.Padding(2)
+        Me.gbInformacionSaca.Size = New System.Drawing.Size(377, 346)
+        Me.gbInformacionSaca.TabIndex = 17
+        Me.gbInformacionSaca.TabStop = False
+        Me.gbInformacionSaca.Text = "Información de la saca"
+        Me.gbInformacionSaca.Visible = False
+        '
+        'gbInformacion
+        '
+        Me.gbInformacion.Controls.Add(Me.lblTiempo)
+        Me.gbInformacion.Controls.Add(Me.LinkLabel1)
+        Me.gbInformacion.Controls.Add(Me.lblNumeroCamion)
+        Me.gbInformacion.Controls.Add(Me.lblNumeroSaca)
+        Me.gbInformacion.Location = New System.Drawing.Point(8, 7)
+        Me.gbInformacion.Margin = New System.Windows.Forms.Padding(2)
+        Me.gbInformacion.Name = "gbInformacion"
+        Me.gbInformacion.Padding = New System.Windows.Forms.Padding(2)
+        Me.gbInformacion.Size = New System.Drawing.Size(143, 90)
+        Me.gbInformacion.TabIndex = 18
+        Me.gbInformacion.TabStop = False
+        Me.gbInformacion.Text = "Información general"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1072, 708)
+        Me.Controls.Add(Me.gbInformacion)
+        Me.Controls.Add(Me.gbInformacionSaca)
+        Me.Controls.Add(Me.gbInformacionCamion)
+        Me.Controls.Add(Me.pbImagenesCamiones)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "Distribuidora de arroz"
+        CType(Me.tbPesoSacas, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbImagenesCamiones, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbSacas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbInformacionCamion.ResumeLayout(False)
+        Me.gbInformacionCamion.PerformLayout()
+        Me.gbInformacionSaca.ResumeLayout(False)
+        Me.gbInformacionSaca.PerformLayout()
+        Me.gbInformacion.ResumeLayout(False)
+        Me.gbInformacion.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TimerDiaHora As Timer
+    Friend WithEvents lblTiempo As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents lblPesoSacas As Label
+    Friend WithEvents tbPesoSacas As TrackBar
+    Friend WithEvents lbCamiones As ListBox
+    Friend WithEvents ilCamiones As ImageList
+    Friend WithEvents pbImagenesCamiones As PictureBox
+    Friend WithEvents lblPesoCamion As Label
+    Friend WithEvents btnCargarSaca As Button
+    Friend WithEvents pbSacas As PictureBox
+    Friend WithEvents ilSacas As ImageList
+    Friend WithEvents pbSaca As ProgressBar
+    Friend WithEvents TimerSaca As Timer
+    Friend WithEvents lblAtencion As Label
+    Friend WithEvents lblNumeroCamion As Label
+    Friend WithEvents lblNumeroSaca As Label
+    Friend WithEvents btnCargarCamion As Button
+    Friend WithEvents lblPesoCamionActual As Label
+    Friend WithEvents gbInformacionCamion As GroupBox
+    Friend WithEvents gbInformacionSaca As GroupBox
+    Friend WithEvents gbInformacion As GroupBox
+End Class
---
+++ Distribuidora_Arroz/Distribuidora_Arroz/Form1.vb
@@ -0,0 +1,165 @@
+﻿Imports System.Threading
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Button
+
+Public Class Form1
+    'Declaracion e inicializacion de variables
+    Dim capacidadCamion As Integer = 0
+    Dim numeroCamiones As Integer = 0
+    Dim pesoCamion As Integer = 0
+    Dim sacasTotal As Integer = 0
+    Dim totales As ArrayList = New ArrayList()
+    Dim subtotales As ArrayList = New ArrayList()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Asignacion del tiempo actual
+        lblTiempo.Text = Now
+        pbImagenesCamiones.Left = 12
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles TimerDiaHora.Tick
+        'Actualizacion del tiempo actual
+        lblTiempo.Text = Now
+    End Sub
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        'Marcamos el enlace como visitado
+        LinkLabel1.LinkVisited = True
+        'Llamamos a un navegador para que se inicia en la pagina web indicada
+        Dim unused = Process.Start("C:\Program Files\Internet Explorer\iexplore.exe", "http://20minutos.com")
+    End Sub
+
+    Private Sub TbPesoSacas_Scroll(sender As Object, e As EventArgs) Handles tbPesoSacas.Scroll
+        'Mostramos el valor actual del trackbar
+        lblPesoSacas.Text = "Peso de saca: " + tbPesoSacas.Value.ToString
+    End Sub
+
+    Private Sub LbCamiones_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbCamiones.SelectedIndexChanged
+        'Indicamos la capacidad del camion elegido
+        lblPesoCamion.Visible = True
+        'En funcion del camion elegido modificamos la imagen, la capacidad y el label que muestra la capacidad 
+        If lbCamiones.SelectedIndex = 0 Then
+            lblPesoCamion.Text = "Peso máximo del camión: " + 18000.ToString + "kg"
+            pbImagenesCamiones.Image = ilCamiones.Images(0)
+            capacidadCamion = 18000
+        ElseIf lbCamiones.SelectedIndex = 1 Then
+            lblPesoCamion.Text = "Peso máximo del camión: " + 23000.ToString + "kg"
+            pbImagenesCamiones.Image = ilCamiones.Images(1)
+            capacidadCamion = 23000
+        ElseIf lbCamiones.SelectedIndex = 2 Then
+            lblPesoCamion.Text = "Peso máximo del camión: " + 28000.ToString + "kg"
+            pbImagenesCamiones.Image = ilCamiones.Images(2)
+            capacidadCamion = 28000
+        End If
+        btnCargarCamion.Enabled = True
+    End Sub
+
+    Private Sub btnCargarSaca_Click(sender As Object, e As EventArgs) Handles btnCargarSaca.Click
+        'Si el peso de la saca indica NO supera la capacidad del camion
+        If capacidadCamion >= pesoCamion + tbPesoSacas.Value Then
+            'Deshabilitamos el boton
+            btnCargarSaca.Enabled = False
+            'Visibilizamos e inicializamos el progeso de como se vacia la saca
+            pbSaca.Visible = True
+            pbSaca.Value = 0
+            'Mostramos que se esta cargando
+            lblAtencion.Visible = True
+            lblAtencion.Text = "Atención: cargando"
+            'Mostramos la imagen de la saca
+            pbSacas.Image = ilSacas.Images(0)
+            'Habilitamos el timer
+            TimerSaca.Enabled = True
+            'Actualizamos la informacion que se muestra: el numero total de sacas, el peso actual del camion
+            sacasTotal += 1
+            pesoCamion += tbPesoSacas.Value
+            'Anadimos el peso de la saca introducida a un arraylist que guarda los subtotales
+            subtotales.Add(tbPesoSacas.Value)
+        Else
+            'Si el peso de la saca indicada supera la capacidad del camion
+            MsgBox("El peso de la saca va a superar la capacidad del camión. Se inicia la carga del siguiente camión. Vuelva a elegir un tipo de camión", vbInformation, "Cambio de camión")
+            'Deshabilitamos el boton de cargar saca hasta que se haya elegido un nuevo camion
+            btnCargarSaca.Enabled = False
+            'Desplazamos la imagen del camion que sale
+            While pbImagenesCamiones.Left > 12
+                pbImagenesCamiones.Left -= 1
+                Thread.Sleep(2)
+            End While
+            'Anadimos el peso del camion a un arraylist que guarda los totales
+            totales.Add(pesoCamion)
+            'Reiniciamos el peso del camion
+            pesoCamion = 0
+            lblPesoCamionActual.Text = "Peso actual del camion: " + pesoCamion.ToString
+            'Invisibilizamos la informacion de la saca
+            gbInformacionSaca.Visible = False
+            pbSaca.Visible = False
+            lblAtencion.Visible = False
+            pbSacas.Image = ilSacas.Images(0)
+            'Habilitamos el boton de cargar camion
+            btnCargarCamion.Enabled = True
+        End If
+    End Sub
+
+    Private Sub TimerSaca_Tick(sender As Object, e As EventArgs) Handles TimerSaca.Tick
+        'Mientras que el proceso de vaciar la saca no se haya terminado
+        If pbSaca.Value < pbSaca.Maximum Then
+            pbSaca.Value += 1
+            lblAtencion.Visible = Not lblAtencion.Visible
+        End If
+        'Cuando se haya terminado de vaciar la saca
+        If pbSaca.Value = pbSaca.Maximum Then
+            'Mostramos que se ha finalizado
+            lblAtencion.Text = "Carga finalizada"
+            lblAtencion.Visible = True
+            pbSacas.Image = ilSacas.Images(1)
+            'Desactivamos el timer
+            TimerSaca.Enabled = False
+            'Actualizamos la informacion mostrada
+            lblNumeroSaca.Text = "Saca nº " + sacasTotal.ToString
+            lblPesoCamionActual.Text = "Peso actual del camion: " + pesoCamion.ToString + " kg"
+        End If
+        'Habilitamos el boton de cargar saca
+        btnCargarSaca.Enabled = True
+    End Sub
+
+    Private Sub btnCargarCamion_Click(sender As Object, e As EventArgs) Handles btnCargarCamion.Click
+        'Deshabilitamos el boton de cargar camion
+        btnCargarCamion.Enabled = False
+        'Si aun no hemos cargado 20 camiones
+        If numeroCamiones < 20 Then
+            numeroCamiones += 1
+            While pbImagenesCamiones.Left < gbInformacionSaca.Left + pbSacas.Left
+                pbImagenesCamiones.Left += 1
+                Thread.Sleep(2)
+            End While
+            lblPesoCamionActual.Text = "Peso actual del camion: " + pesoCamion.ToString
+            lblNumeroCamion.Text = "Camion nº " + numeroCamiones.ToString
+            btnCargarSaca.Enabled = True
+        Else
+            'Mostramos la informacion de haber finalizado junto a todos los totales y subtotales
+            Dim salida As String = "No cargue más sacas. Se despache el camión 20. FINALIZADA LA CARGA DEL DIA. " & Chr(10) & " Totales (peso de cada camión): "
+            For Each total In totales
+                salida = salida.Insert(salida.Length, Chr(10) & total.ToString & " kg").ToString
+            Next
+            salida = salida.Insert(salida.Length, Chr(10) & "Subtotales (peso de cada saca): ")
+            For Each subtotal In subtotales
+                salida = salida.Insert(salida.Length, Chr(10) & subtotal.ToString & " kg" & Chr(10))
+            Next
+            MsgBox(salida, vbInformation, "Distribución de arroz")
+        End If
+        'Se muestra el groupbox que contiene los componentes relacionados con la carga de sacas
+        gbInformacionSaca.Visible = True
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim salida As String = "Se finaliza la carga del día." & Chr(10) & "Totales (peso de cada camión): "
+        If pesoCamion > 0 Then
+            totales.Add(pesoCamion)
+        End If
+        'Mostramos la informacion de haber finalizado junto a todos los totales y subtotales
+        For Each total In totales
+            salida = salida.Insert(salida.Length, Chr(10) & total.ToString & " kg").ToString
+        Next
+        salida = salida.Insert(salida.Length, Chr(10) & "Subtotales (peso de cada saca): ")
+        For Each subtotal In subtotales
+            salida = salida.Insert(salida.Length, Chr(10) & subtotal.ToString & " kg")
+        Next
+        MsgBox(salida, vbInformation, "Distribución de arroz")
+    End Sub
+End Class
